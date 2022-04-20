bbb4cfcbfaccb4b87478ee22db3e0b2671f21251
Joffre Hermosilla Salas <joffre.hermosilla@gmail.com>
2022-04-20 09:31:31 -0500

    programancion funcional Reactor lambdas

--- microservicio-activo-personal/src/main/java/nttdata/bootcamp/microservicios/activo/personal/documents/PersonalAsset.java
+++ microservicio-activo-personal/src/main/java/nttdata/bootcamp/microservicios/activo/personal/documents/PersonalAsset.java
@@ -11,12 +11,22 @@ import org.springframework.format.annotation.DateTimeFormat;
 import lombok.Data;
 
 @Data
-@Document(collection = "personal-asset")
+@Document(collection = "active-account-personal-asset")
 public class PersonalAsset {
 	@Id
 	private String id;
 	
-	private Double totalAmount;
+    	
+	
+	//cantidad de credito que tiene la cuenta peronal
+	private Double allowedperclient;
+	
+	private boolean enabledtouse;
+	
+	private int paymentdays;
+	
+	private Double currentbalance;
+	
 	
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
